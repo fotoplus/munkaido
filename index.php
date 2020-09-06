78590c71cb81dee7af4f02d9b778d579082b989c
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-06 19:07:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,43 @@
+<?php
+
+
+?>
 <!doctype html>
 <html class="no-js" lang="hu">
 
   <head>
 
     <meta charset="utf-8">
-    <title>Munkaidőnyilvántartás</title>
 
+    <title>Munkaidő nyilvántartás</title>
     <meta name="description" content="Online munkaidőnyilvántartás">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="style/normalize.css">
-    <link rel="stylesheet" href="style/main.css">
+
+    <link rel="stylesheet" href="styles/normalize/normalize.css">
+    <link rel="stylesheet" href="styles/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="styles/reset/reset.css">
+
+    <link rel="stylesheet" href="styles/main.css">
 
   </head>
 
   <body>
 
+    <?php
+
+
+
+    ?>
 
     <!-- Scriptek -->  
-      <script src="js/modernizr/modernizr.js"></script>
+      <script src="js/modernizr/modernizr-custom.js"></script>
       <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- Scriptek (vége) -->
 
   </body>
 </html>
+<?php
+
+?>

6e3a2d86509e3e65f129480eeccc1d299b30dec5
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-06 16:46:15 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="hu">
+
+  <head>
+
+    <meta charset="utf-8">
+    <title>Munkaidőnyilvántartás</title>
+
+    <meta name="description" content="Online munkaidőnyilvántartás">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="style/normalize.css">
+    <link rel="stylesheet" href="style/main.css">
+
+  </head>
+
+  <body>
+
+
+    <!-- Scriptek -->  
+      <script src="js/modernizr/modernizr.js"></script>
+      <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <!-- Scriptek (vége) -->
+
+  </body>
+</html>

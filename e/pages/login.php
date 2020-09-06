d48e65d189e7732efea7f90b5d5c495a618cc6ec
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-06 19:41:49 +0200

    Create login.php

---
+++ e/pages/login.php
@@ -0,0 +1,10 @@
+<?php
+?>
+
+<form method="post" name="fotoplus-login">
+
+  <input type="text" name="username" />
+  <input type="password" name="password" />
+  <input type="submit" name="">
+
+</form>

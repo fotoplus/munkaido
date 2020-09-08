170b568a9a6f04b65048ae671b4bfd58f7cdf144
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-08 16:11:09 +0200

    Update login.php

--- e/pages/login.php
+++ e/pages/login.php
@@ -1,10 +1,20 @@
 <?php
 ?>
 
-<form method="post" name="fotoplus-login">
+<form method="post" name="fotoplus-login" class="rounded border">
 
-  <input type="text" name="username" />
-  <input type="password" name="password" />
-  <input type="submit" name="">
+  <fieldset>
+    <legend>Jelentkezz be!</legend>
 
+    <label for="username">Felhasználóinév / e-mail cím</label>
+    <input type="text" name="username" />
+
+    <label for="jelszó">Jelszó</label>
+    <input type="password" name="password" />
+    
+    <input type="submit" name="login" value="Jelentkezzünk be">
+
+    <a href="./elfelejtett-jelszo" title="Kattimts ide ha elfelejtetted a jelszavad.">Elfelejtettem a jelszavam</a>
+
+  </fieldset>
 </form>

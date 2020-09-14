4d7fed43bde0273bd575deaf977955153ab581da
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-14 21:40:21 +0200

    Create sample_mysql.php

---
+++ e/credentials/sample_mysql.php
@@ -0,0 +1,15 @@
+<?PHP # Elvárt fájlnév: mysql.php
+
+/**
+ * MySQL
+ * Az adabázis kapcsolat adatai
+ * További beállítások: /e/config/mysql.php
+ */
+$mysql = Array(
+  'host' => '',
+  'user' => '',
+  'password' => '',
+  'db' => ''
+);
+
+?>

732a1e2a834de94cc9a4936332190d635991b917
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-15 00:05:09 +0200

    Update mysql.php

--- e/modules/mysql/mysql.php
+++ e/modules/mysql/mysql.php
@@ -1,46 +1,27 @@
 <?php
 
-/**
- * Kapcsolódási adatok: /e/credentials/mysql.php
- * További beállítások: /e/config/mysql.php
- */
-
-include('e/credentials/mysql.php');
-include('e/config/mysql.php');
+require_once ('e/credentials/mysql.php'); // Kapcsolódási adatok
+require_once ('e/config/mysql.php'); // További beállítások
 
 /**
- * Az adatbáziskapcsolat létrehozása
- * 
+ * Kísérlet az adatbáziskapcsolat létrehozására
  */
-$mysqli = new mysqli(
+try {
+
+  $mysqli = new mysqli(
     $mysql['host'], // Az adatbázis címe, jellemzően "localhost"
     $mysql['user'], // Felhasználóinév
     $mysql['password'], // Jelszó
     $mysql['db'] // A hasznáűlni kívánt adatbáis
   );
+  
+  $mysqli->set_charset($db_charset);
 
+} catch(Exception $e) {
 
-/*
- * Ez lenne a "hivatalos" megoldás a kapcsolat ellenőrzésére,
- * viszont a PHP 5.2.9 verziója alatt nem igazán működik.
- */
-if ($mysqli->connect_errno) {
-  echo "Sikertelen kapcsolódás az adatbázishoz: " . $mysqli->connect_error;
-  exit();
+  exit( 'Nem sikerült létrehozni a kapcsolatot az adatbázissal, az adatbázis válasza: ' . $e->getMessage() ); 
+
 }
-
-
-/**
- * Az adatbázis-kapcsolat karakterkódolásának ellenőrzése
- * és szükség esetén megváltoztatása a megfelelőre.
- */
-if($mysqli->character_set_name() != $db_charset) {
-  if (!$mysqli->set_charset($db_charset)) {
-    printf("Error loading character set utf8: %s\n", $mysqli->error);
-    exit();
-  }
-}
-
 
 
 ?>

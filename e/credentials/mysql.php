6692563b6287b99c8651bf943f91051379f49609
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-14 21:23:58 +0200

    Update mysql.php

---
+++ e/credentials/mysql.php
@@ -0,0 +1,15 @@
+<?PHP
+
+/**
+ * MySQL
+ * Az adabázis kapcsolat adatai
+ * További beállítások: /e/config/mysql.php
+ */
+$mysql = Array(
+  'host' => '',
+  'user' => '',
+  'password' => '',
+  'db' => '',
+);
+
+?>

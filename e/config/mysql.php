2172f0ff97d977f2bdf1eed26611c88cfb478559
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-15 00:05:03 +0200

    Update mysql.php

--- e/config/mysql.php
+++ e/config/mysql.php
@@ -1,7 +1,12 @@
 <?php
 
+// A mysqli hibakijelzése
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$db_charset = 'utf8';
+// A kapcsolat karakterkódolása (megegyezik az adatbáziséval)
+$db_charset = 'utf8mb4_unicode_ci';
+
+// Adatbázis tábláinak előtagja (ha van ilyen)
 $tbl_prefix = 'fp_';
 
 

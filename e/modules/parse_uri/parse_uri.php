dc4d348668b3bcfaf747a8ef17781f63723409c8
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-26 22:51:10 +0200

    Update parse_uri.php

--- e/modules/parse_uri/parse_uri.php
+++ e/modules/parse_uri/parse_uri.php
@@ -1,44 +1,50 @@
 <?PHP
-
-/**
+/** AZ URI feldolgozása
  * 
- * 
-    $page = array();
-    $page['name'] = isset( $_REQUEST['page'] ) ? $_REQUEST['page'] : 'home';
-
-    print
-
-
+ * Bővebben: README.md
  */
-
-
 
 
  class ParseURI {
 
   public $segments;
+  public $page;
+  protected $pages_dir = './e/pages/';
 
   function __construct() {
+
     $_SERVER['REQUEST_URI_PATH'] = preg_replace('/\?.*/', '', $_SERVER['REQUEST_URI']);
     $segments = explode('/', trim($_SERVER['REQUEST_URI_PATH'], '/'));
     $segments = array_slice ($segments, URI_IGNORE);
+
     print_r($segments);
 
   }
   
 
-  function is_page() {
+  function is_page($page_check) {
+
+    $page_check = isset( $page_check ) ? $page_check : $segments[0];
+    $page_file = $pages_dir . $page_check . '.php';
+    
+    if( file_exists() ) {
+
+    }
+
+    unset($page_file);
 
   }
 
-  function open_page () {
+  function page() {
+
+    $page['name'] = isset( $segments[0] ) ? $segments[0] : 'home';
 
   }
 
  }
 
 
- $page=new ParseURI(URI_IGNORE);
+ $page=new ParseURI();
 
 
 

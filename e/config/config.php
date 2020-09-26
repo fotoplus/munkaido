2af35d6d75c42f2d7c12e145fafd9488b18180f6
Borbás Mátyás <25402691+borbasmatyas@users.noreply.github.com>
2020-09-26 22:51:16 +0200

    Update config.php

--- e/config/config.php
+++ e/config/config.php
@@ -1,27 +1,24 @@
 <?php
 
-### MŰKÖDÉSI BEÁLLÍTÁSOK
 
 /** PHP hibakijelzés
- *  https://www.php.net/manual/en/errorfunc.configuration.php
- */
- #error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
- error_reporting(E_ALL);
- ini_set("display_errors", 1); 
-
-# A működési beállítások vége
-
-
-
-
-
-### OLDAL BEÁLLÍTÁSOK
-
-/** Az URI elejének figyelmenkívülhagyása > /e/modules/parse_uri
  * 
+ * Bővebben: https://www.php.net/manual/en/errorfunc.configuration.php
  *
  */
-  define("URI_IGNORE", 4);
+#error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+error_reporting(E_ALL);
+ini_set("display_errors", 1); 
+
+
+
+/** ParseURI / URI_IGNORE
+ * 
+ * Az URI elejének figyelmenkívülhagyása, ha szükséges
+ * Bővebben: /e/modules/parse_uri/README.md
+ * 
+ */
+define("URI_IGNORE", 4);
 
 
 ?>
